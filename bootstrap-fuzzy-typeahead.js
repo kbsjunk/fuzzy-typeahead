3143cdaf79c4fe6868030c085a03b9fe8e84fb55
KitBS <kit.burton.senior@gmail.com>
2013-06-12 16:15:45 +1100

    Added joinHighlight option

--- bootstrap-fuzzy-typeahead.js
+++ bootstrap-fuzzy-typeahead.js
@@ -3,14 +3,19 @@ function fuzzyMatcher(item, query) {
         var regex = new RegExp("^" + pattern, "img");
         return regex.test(item);
 }
-function fuzzyHighlighter(item, query) {
+function fuzzyHighlighter(item, query, joinHighlight) {
     var pattern = query.split("");
-        var replace = '';
+    var replace = '';
+    if (joinHighlight === true) {
+        replace = "<b>$1</b>$2";
+        pattern = "(" + pattern.join(".*") + ")(.*)";
+    } else {
         for (var i = 0; i < pattern.length; i++) {
             var j = i * 2;
             replace = replace + "<b>" + "$" + (j + 1) + "</b>" + "$" + (j + 2);
         }
         pattern = "(" + pattern.join(")(.*)(") + ")(.*)";
-        var regex = new RegExp("^" + pattern, "img");
-        return item.replace(regex, replace);
+    }
+    var regex = new RegExp("^" + pattern, "img");
+    return item.replace(regex, replace);
 }
